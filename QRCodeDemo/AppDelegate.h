541cdeb3fdc6ba4ada3421063317c69b10b68974
Xin Sun <Xin@xindembp.home>
2015-11-04 11:45:08 -0500

    Initial Commit

---
+++ QRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeDemo
+//
+//  Created by Xin Sun on 11/4/15.
+//  Copyright © 2015 Xin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeDemo
+//
+//  Created by Xin Sun on 11/4/15.
+//  Copyright © 2015 Xin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
